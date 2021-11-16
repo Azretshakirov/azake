6a644c78d2d5d4bcbb52338a3270549fdadd339b
Azretshakirov <92852391+Azretshakirov@users.noreply.github.com>
2021-11-16 20:28:01 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+<title>Фоновое изображение</title>
+
+ </head>
+ <body bgcolor=white background="https://kartinkin.net/uploads/posts/2021-07/1626254156_20-kartinkin-com-p-korolevskii-fon-krasivo-30.jpg">
+
+<center><h1>PORTFOLIO</h1></center>
+
+<hr>
+
+<h2>AZRET</h2>
+
+<p><img src="https://thumb.tildacdn.com/tild3535-3966-4038-a163-636265636439/-/resize/560x/-/format/webp/IMG_1581.JPG" alt="portfolio image" width="220" height="200" style= float:right></p>
+
+<hr>
+
+<p>Name  :   Azret      </p>
+<hr>
+<p>Surname:   Shakirov     </p>
+<hr>
+<p>Address    :   Bugu-Ene 63            </p>
+<hr>
+<p>Class  :  Csc-20a       </p>
+<hr>
+
+</body>
+</html>
